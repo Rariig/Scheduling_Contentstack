78a1806a24032417acf4b2122ce668b33bef817f
rariig <raoul.rocco@gmail.com>
2022-12-27 01:53:46 +0200

    Can calculate days difference from start and endDate input.

--- scheduling.js
+++ scheduling.js
@@ -1,8 +1,11 @@
-function generateDSL();
+function generateDSL()
 {
-    const endDate = document.getElementById("endDate").getDate()
-    /*const startDate =  document.getElementById("startDate").value
-    var daysToRun = endDate. */
-
-    document.getElementById("DSLString") = endDate;
+    var startDate = new Date (document.getElementById("startDate").value)
+    var endDate = new Date (document.getElementById("endDate").value)
+    var difference = endDate.getTime() - startDate.getTime()
+      
+    var differenceInDays = difference / (1000 * 3600 * 24)
+    
+    console.log(differenceInDays)
+    document.getElementById("DSLString").textContent = differenceInDays
 }
