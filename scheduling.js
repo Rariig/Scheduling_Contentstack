c81e0293e68f32d3438dd19038877c1371041f1e
Raoul Rocco Riigov <rariig@ttu.ee>
2023-01-19 04:41:08 +0200

    Additional settings opacity, pointer events added. Can generate DSL from base case and a selection of  weekdays.

--- scheduling.js
+++ scheduling.js
@@ -10,14 +10,12 @@ function generateDSL()
     var startTime = document.getElementById("startTime").value
     var endTime = document.getElementById("endTime").value
 
-    
-    
-    
-  
-    let finalDSL = document.getElementById("DSLString").textContent
+    var finalDSL = document.getElementById("DSLString").textContent
     finalDSL = "1 2 3 4 5_x x_"+differenceInDays
     finalDSL = finalDSL.replace(finalDSL.split("_")[1].split(" ")[0], startTime)
     finalDSL = finalDSL.replace(finalDSL.split("_")[1].split(" ")[1], endTime)
+
+    console.log(finalDSL)
     
 
 
@@ -25,20 +23,23 @@ function generateDSL()
       const weekdays = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"];
       const selectedWeekdays = Array.from(document.getElementsByClassName("weekday"))
                       .filter(weekday => weekday.checked)
-                      .map((weekday) => weekday.id.split("-")[1].toUpperCase());
-      console.log(selectedWeekdays);
+                      .map((weekday) => weekday.id.split("-")[1].toUpperCase())
+      console.log(selectedWeekdays)
       
       //Update DSL string
-      finalDSL = document.getElementById("DSLString").textContent;
+      console.log(finalDSL)
       if (selectedWeekdays.length === 0) {
          finalDSL = finalDSL.replace(finalDSL.split("_")[0].split(" ")[4], "*");
       } else {
           finalDSL = finalDSL.replace(finalDSL.split("_")[0].split(" ")[4], selectedWeekdays);
       }
-      document.getElementById("DSLString").textContent = finalDSL;  
 
 
+      console.log(finalDSL)
+      finalDSL = finalDSL.replace(finalDSL.split("_")[0].split(" ")[2], "*");
+      finalDSL = finalDSL.replace(finalDSL.split("_")[0].split(" ")[3], "*");
 
+      document.getElementById("DSLString").textContent = finalDSL
 
    /* //find months to run the task on
     const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
@@ -78,3 +79,31 @@ flatpickr(dateInput, {
     selectedDate = dateStr;
   }
 });
+
+
+var additionalSettingsCheckbox = document.getElementById("additional-settings-checkbox");
+var additionalSettings = document.getElementsByClassName("additional-settings");
+var weekdayCheckboxes = document.getElementsByClassName("weekday");
+
+additionalSettingsCheckbox.addEventListener("change", function() {
+    if (this.checked) {
+        for (var i = 0; i < additionalSettings.length; i++) {
+            additionalSettings[i].style.opacity = "1";
+            additionalSettings[i].style.pointerEvents = "auto";
+        }
+        for (var i = 0; i < weekdayCheckboxes.length; i++) {
+            weekdayCheckboxes[i].checked = false;
+        }
+    } else {
+        for (var i = 0; i < additionalSettings.length; i++) {
+            additionalSettings[i].style.opacity = "0.5";
+            additionalSettings[i].style.pointerEvents = "none";
+        }
+        for (var i = 0; i < weekdayCheckboxes.length; i++) {
+            weekdayCheckboxes[i].checked = false;
+        }
+    }
+});
+
+
+
